f0663b3a21c0a075f349b2f1e0ff7659a9882e09
LinkPark2016 <751081758@qq.com>
2017-08-24 20:54:27 +0800

    无限极分类等

--- dingcan/application/admin/controller/Product.php
+++ dingcan/application/admin/controller/Product.php
@@ -2,54 +2,99 @@
 	namespace app\admin\controller;
 	use app\admin\controller\Auth;
 	use app\admin\model\Product as ProductModel;
-
+	use think\Request;
 	class Product extends Auth{
 		protected $is_login = ['*'];
-		protected $user;
+		protected $product;
 		public function _initialize()
 		{
 			parent::_initialize();
-			$this->user = new ProductModel();
+			$this->product = new ProductModel();
 		}
-		//菜品管理
-		public function product_brand()
-		{
-			return $this->fetch();
-		}
-		//菜品分类管理
+
+		//菜品分类展示
 		public function product_category()
 		{
 			//分类展示
-			$result = $this->user->showcai();
+			$result = $this->product->showcai();
 			if ($result) {
-				$xing['b_id'] = [];
+				$xing = [];
 				$this->assign('result', $result);
 				foreach ($result as $k => $v) {
-					$xing = str_repeat('★', substr_count($v['path'], ','));
-					
-					$this->assign('xing', $xing);
-					dump($xing);
+					$xing[]['b_id'] = str_repeat('&nbsp;&nbsp;&nbsp; ★|--&nbsp;&nbsp;&nbsp; ', substr_count($v['path'], ','));
 				}
-				
-				die;
-				dump($xing);
+				$this->assign('xing', $xing);
 			} else {
 				$this->assign('result', '');
 			}
-			// dump($result);die;
 			return $this->fetch();
 		}
-		//产品管理
+		//分类分页管理
 		public function product_list()
 		{
+			$list = $this->product->showpage();
+			// dump($result);die;
+			if ($list) {
+				// $page = $list->render();
+				$this->assign('list', $list);
+			} else {
+				$this->assign('list', '');
+			}
 			return $this->fetch();
 		}
-		//添加菜品
-		public function product_category_add()
+		//添加分类
+		public function product_add()
 		{
+			$data = [
+				'b_id' => input('sel'),
+				'bname' => input('bname')
+			];
 			
+				$result = $this->product->addp($data);
+			if ($result) {
+				$this->success('添加分类成功');
+			} else {
+				$this->error('添加分类成功');
+			}
+		}
+		//展示当前分类
+		public function product_man()
+		{
+			$data = input('id');
+			$result = $this->product->lei($data);
+			if ($result) {
+				$this->assign('result', $result);
+			} else {
+				$this->assign('result', '');
+			}
 			return $this->fetch();
 		}
-		
-		
+		//修改分类
+		public function updlei()
+		{
+			$data = [
+				'b_id' => input('b_id'),
+				'b_name' => input('b_name')
+			];
+			$result = $this->product->updlei($data);
+
+			if ($result) {
+				$this->success('修改成功', 'product/product_list');
+			} else {
+				$this->error('修改失败');
+			}
+		}
+		//删除分类
+		public function dellei()
+		{
+			// return json(0);
+			// return json(input('b_id'));
+			$data = input('b_id');
+			$result = $this->product->dellei($data);
+			if ($result) {
+				return json(1);
+			} else {
+				return json(0);
+			}
+		}
 	}
--- dingcan/application/admin/model/Product.php
+++ dingcan/application/admin/model/Product.php
@@ -2,17 +2,96 @@
 	namespace app\admin\model;
 	use think\Model;
 	use think\Db;
+	use traits\model\SoftDelete;
+	use app\admin\model\Bk as Bk;
 	class Product extends Model{
-
-		//菜品分类展示
+		use SoftDelete;
+		protected $deleteTime	= 'delete_time';
+		//菜品分类查询展示
 		public function showcai()
 		{	
-			$result = Db::name('bk')->select();
+			$result = Db::name('bk')->where('delete_time','null')->order('concat(path, b_id)')->select();
 			if ($result) {
 				return $result;
 			} else {
 				return false;
 			}
 		}
-
+		//菜品分类分页查询
+		public function showpage()
+		{	
+			$result = Db::name('bk')->where('delete_time','null')->order('concat(path, b_id)')->paginate(5);
+			if ($result) {
+				return $result;
+			} else {
+				return false;
+			}
+		}
+		//添加菜品分类
+		public function addp($data)
+		{
+			//添加顶级
+			if (!$data['b_id']) {
+				$datas = [
+					'b_name' => $data['bname'],
+					'parentid' => 0,
+					'path' => '0,'
+				];
+				$result = Db::name('bk')->insert($datas);
+				// dump($result);die;
+			//添加子级
+			} else {
+				//path路径，
+				$path = Db::name('bk')->where('b_id',$data['b_id'])->find();
+				// dump($path);die;
+				$datas = [
+					'b_name' => $data['bname'],
+					'parentid' => $path['b_id'],
+					'path' => $path['path'] . $path['b_id'] . ','
+				];
+				// dump($datas);die;
+				$result = Db::name('bk')->insert($datas);
+			}
+			if ($result) {
+				return $result;
+			} else {
+				return false;
+			}
+			
+		}
+		//具体类别查询
+		public function lei($data)
+		{
+			$result = Db::name('bk')->where('b_id', $data)->find();
+			if ($result) {
+				return $result;
+			} else {
+				return false;
+			}
+		}
+		//类别修改
+		public function updlei($data)
+		{
+			$result = Db::name('bk')->where('b_id', $data['b_id'])->update(['b_name'=>$data['b_name']]);
+			if ($result) {
+				return $result;
+			} else {
+				return false;
+			}
+		}
+		//删除分类
+		public function dellei($data)
+		{
+			// $result = Db::name('bk')->where('b_id', $data)->delete();
+			// $result = Db::name('bk')->where('b_id', $data)->destroy();
+			$result = db('bk')->where('b_id', $data)->destroy();
+			// $result = model('Bk')->where('b_id', $data)->destroy();
+			// $result = $bk->where('b_id', $data)->destroy();
+			// dump($result);
+			if ($result) {
+				return $result;
+			} else {
+				return false;
+			}
+		}
 	}
