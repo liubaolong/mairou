53808f9b5c6197603d3412d6a0b9a5f6b9d91c51
liubaolong <longge8521>
2017-08-25 20:41:02 +0800

    页面

--- dingcan/application/index/model/Index.php
+++ dingcan/application/index/model/Index.php
@@ -10,9 +10,39 @@ class Index extends Model
 		$result = Db::name('meal')->where('m_isput',0)->order('m_mothcounts','desc')->limit(3)->select();
 		return $result;
 	} 
+	//查询菜品所属商铺
 	static public function store($data)
 	{
 		$result = Db::name('shop')->where('delete_time',null)->where('s_status',1)->where('s_qu',$data)->limit(3)->select();
+		return $result;
+	}
+	//查询所有的店铺
+	static public function alldish()
+	{
+		$result = Db::table('f_shop')->where('delete_time',null)->paginate(4);
+		return $result;
+	}
+	//查询销量店铺
+	static public function sale()
+	{
+		$result = Db::table('f_shop')->where('delete_time',null)->order('s_monthcount','desc')->paginate(4);
+		return $result;
+	}
+	//查询注册时间
+	static public function stime()
+	{
+		$result = Db::table('f_shop')->where('delete_time',null)->order('s_addtime','desc')->paginate(4);
+		return $result;
+	}
+	//查询积分菜
+	static public function credits()
+	{
+		$result  = Db::table('f_meal')
+					->alias('m')
+					->join('f_shop s','s.s_id = m.sid')
+					->where('m.delete_time',null)
+					->where('m.m_score','neq',0)
+					->paginate(9);
 		return $result;
 	}
 }
